2788c603cfeb1f657c2e6113ddc19714d88661bf
jay947462 <jakasofu@binghamton.edu>
2026-02-11 17:42:58 -0500

    corrected handlecommand

--- Control/src/relays/relays.cpp
+++ Control/src/relays/relays.cpp
@@ -110,8 +110,8 @@ void Relays::handleCommand(RelaysCommand command) {
     backRightMotor.handleCommand(command.back_right_motor);
     frontLeftMotor.handleCommand(command.front_left_motor);
     frontRightMotor.handleCommand(command.front_right_motor);
-    leftDampeningMotor.handleCommand(command.middle_left_motor);
-    rightDampeningMotor.handleCommand(command.middle__right_motor);
+    middleLeftMotorMotor.handleCommand(command.middle_left_motor);
+    middleRightMotor.handleCommand(command.middle_right_motor);
     arm.handleCommand(command.arm);
     science.handleCommand(command.science);
 }
